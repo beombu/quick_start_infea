6f581a7f25607d6db325aa1d1ce0352ddda32818
beombu <beombu13@gmail.com>
2023-10-06 22:46:40 +0900

    :tada: 프로젝트 시작

---
+++ src/main/java/com/example/quick_start_infra/QuickStartInfraApplication.java
@@ -0,0 +1,13 @@
+package com.example.quick_start_infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickStartInfraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickStartInfraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quick_start_infra/QuickStartInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quick_start_infra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickStartInfraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
